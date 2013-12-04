49e25232b1d08ff9fceb195ea9901ab33dd185a2
kh2ouija <kh2ouija@yahoo.com>
2013-12-04 17:26:36 +0200

    updated to fest-assert-2.x

--- src/test/java/legacy/InnTest.java
+++ src/test/java/legacy/InnTest.java
@@ -2,24 +2,25 @@ package legacy;
 
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.extractProperty;
 
 public class InnTest {
     Inn inn = new Inn();
 
     @Test
     public void should_list_items() {
-        assertThat(inn.getItems()).onProperty("name").containsExactly("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake");
-        assertThat(inn.getItems()).onProperty("quality").containsExactly(20, 0, 7, 80, 20, 6);
-        assertThat(inn.getItems()).onProperty("sellIn").containsExactly(10, 2, 5, 0, 15, 3);
+        assertThat(extractProperty("name").from(inn.getItems())).containsExactly("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake");
+        assertThat(extractProperty("quality").from(inn.getItems())).containsExactly(20, 0, 7, 80, 20, 6);
+        assertThat(extractProperty("sellIn").from(inn.getItems())).containsExactly(10, 2, 5, 0, 15, 3);
     }
 
     @Test
     public void should_update_items() {
         inn.updateQuality();
 
-        assertThat(inn.getItems()).onProperty("quality").containsExactly(19, 1, 6, 80, 21, 5);
-        assertThat(inn.getItems()).onProperty("sellIn").containsExactly(9, 1, 4, 0, 14, 2);
+        assertThat(extractProperty("quality").from(inn.getItems())).containsExactly(19, 1, 6, 80, 21, 5);
+        assertThat(extractProperty("sellIn").from(inn.getItems())).containsExactly(9, 1, 4, 0, 14, 2);
     }
 
     @Test
@@ -27,8 +28,8 @@ public class InnTest {
         inn.updateQuality();
         inn.updateQuality();
 
-        assertThat(inn.getItems()).onProperty("quality").containsExactly(18, 2, 5, 80, 22, 4);
-        assertThat(inn.getItems()).onProperty("sellIn").containsExactly(8, 0, 3, 0, 13, 1);
+        assertThat(extractProperty("quality").from(inn.getItems())).containsExactly(18, 2, 5, 80, 22, 4);
+        assertThat(extractProperty("sellIn").from(inn.getItems())).containsExactly(8, 0, 3, 0, 13, 1);
     }
 
     @Test
@@ -37,8 +38,8 @@ public class InnTest {
             inn.updateQuality();
         }
 
-        assertThat(inn.getItems()).onProperty("quality").containsExactly(0, 50, 0, 80, 0, 0);
-        assertThat(inn.getItems()).onProperty("sellIn").containsExactly(-40, -48, -45, 0, -35, -47);
+        assertThat(extractProperty("quality").from(inn.getItems())).containsExactly(0, 50, 0, 80, 0, 0);
+        assertThat(extractProperty("sellIn").from(inn.getItems())).containsExactly(-40, -48, -45, 0, -35, -47);
     }
 
     /*@Test
